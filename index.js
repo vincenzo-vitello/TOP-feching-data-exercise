ed81feceb411f4a14007944a6ed68ce395d91067
Vincenzo Vitello <vincenzo.vitello@icloud.com>
2025-01-26 15:58:34 +0100

    refactored to use async/await

--- index.js
+++ index.js
@@ -7,24 +7,47 @@ let userInputValue = "";
 const API_KEY = "CITDCZJGLyScw6rPoqbny218Sl8r16WW";
 const API_URL = `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}`;
 
-function getData(search) {
-    fetch(`${API_URL}&s=${search}`, { mode: 'cors' })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`Errore ${response.status}: Impossibile caricare la GIF`);
-            }
-            return response.json();
-        })
-        .then((data) => {
+// function getData(search) {
+//     fetch(`${API_URL}&s=${search}`, { mode: 'cors' })
+//         .then((response) => {
+//             if (!response.ok) {
+//                 throw new Error(`Error ${response.status}: Can't load gifs.`);
+//             }
+//             return response.json();
+//         })
+//         .then((data) => {
+//             if(data.data != []){
+//                 img.src = data.data.images.original.url;
+//                 img.alt = search;
+//             } else {
+//                 alert('wait what? There is nothing like that')
+//             }
+//         })
+//         .catch((error) => {
+//             console.error(error.message);
+//             img.alt = "Ops, there was an error.";
+//         });
+// }
+async function getData(search) {
+    try {
+        const response = await fetch(`${API_URL}&s=${search}`, { mode: "cors" });
+        if(!response.ok){
+            throw new Error(`Error ${response.status}: Can't load gifs.`)
+        }
+        const data = await response.json()
+        if(data.data && data.data.images){
             img.src = data.data.images.original.url;
-            img.alt = search;
-        })
-        .catch((error) => {
-            console.error(error.message);
-            img.alt = "Ops, there was an error.";
-        });
+            img.alt = search
+        } else {
+            alert('Sorry, there is nothing like that...')
+        }
+            
+    }
+    catch (error) {
+        console.log('Something went wrong', error.message)
+        img.alt = "Oops, there was an error.";
+    }
 }
-
 window.addEventListener('load', () => {
     getData(startingSearch);
 });
@@ -39,6 +62,6 @@ form.addEventListener('submit', (e) => {
     userInputValue = userInput.value.trim();
     if (userInputValue) {
         getData(userInputValue);
-        form.reset();
+        form.reset(); 
     }
 });
